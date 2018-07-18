141d2398b3bcfbcd6b5cd9573319dae4f08a5716
Manas Shukla <mshukla@apple.com>
2018-07-18 00:56:43 -0700

    Moving top to take

--- task1/src/main/scala/FollowerRDD.scala
+++ task1/src/main/scala/FollowerRDD.scala
@@ -13,7 +13,7 @@ object FollowerRDD {
     val data = followerRDD.map(line => (line.split("\t")(0), 1))
       .reduceByKey(_+_)
       .sortBy(line => line._2,false)
-      .top(100).map(line => line._1+"\t"+line._2)
+      .take(100).map(line => line._1+"\t"+line._2)
 
 
     sc.parallelize(data).saveAsTextFile("wasb:///followerRDD-output")
