47b906f3b73d726753c9f580b1f333830714887d
Manas Shukla <mshukla@apple.com>
2018-07-19 01:47:54 -0700

    Getting the spark context from spark session

--- task1/src/main/scala/FollowerDF.scala
+++ task1/src/main/scala/FollowerDF.scala
@@ -13,14 +13,14 @@ object FollowerDF {
       .config("spark.some.config.option", "Task1")
       .getOrCreate()
 
-    val conf = new SparkConf()
-    val sc = new SparkContext(conf)
+    val sc = spark.sparkContext
     val followerRDD = sc.textFile("wasb://spark@cmuccpublicdatasets.blob.core.windows.net/Graph")
 
     import spark.implicits._
     val columns = Array("followee","follower")
     val dataframe = followerRDD.map(line => (line.split("\t")(1), line.split("\t")(0))).toDF(columns: _*)
     val finaldf = dataframe.groupBy("followee").count().orderBy(desc("count")).limit(100)
+    finaldf.show()
     finaldf.write.format("parquet").save("wasb:///followerDF-output")
   }
 
