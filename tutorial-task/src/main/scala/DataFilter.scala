88b1a67fce6b664d6b6bb5b59764d4b8131f89a9
Manas Shukla <mshukla@apple.com>
2018-07-20 00:41:11 -0700

    Correcting the threshold

--- tutorial-task/src/main/scala/DataFilter.scala
+++ tutorial-task/src/main/scala/DataFilter.scala
@@ -210,7 +210,7 @@ object DataFilter {
       totalcount=totalcount+1
     }
     //return..
-    if(totalcount >1000000 ) (totalcount,arr) else null
+    if(totalcount >100000) (totalcount,arr) else null
   }
 
 }
