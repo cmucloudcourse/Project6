fb490155ec20a26db3ff853bf9698e73c5619509
Manas Shukla <mshukla@apple.com>
2018-07-22 22:57:41 -0700

    First Commit for page rank

---
+++ task2/src/main/scala/PageRank.scala
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// scalastyle:off println
+package org.apache.spark.examples
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Computes the PageRank of URLs from an input file. Input file should
+  * be in format of:
+  * URL         neighbor URL
+  * URL         neighbor URL
+  * URL         neighbor URL
+  * ...
+  * where URL and their neighbors are separated by space(s).
+  *
+  * This is an example implementation for learning how to use Spark. For more conventional use,
+  * please refer to org.apache.spark.graphx.lib.PageRank
+  *
+  * Example Usage:
+  * {{{
+  * bin/run-example SparkPageRank data/mllib/pagerank_data.txt 10
+  * }}}
+  */
+object PageRank {
+
+
+  def main(args: Array[String]) {
+
+    val INPUT_FILE = "wasb://spark@cmuccpublicdatasets.blob.core.windows.net/Graph"
+    val OUTPUT_FILE = "wasb:///pagerank-output"
+
+    val spark = SparkSession
+      .builder
+      .appName("PageRank")
+      .getOrCreate()
+
+    val iters = if (args.length > 1) args(1).toInt else 10
+    val lines = spark.read.textFile(INPUT_FILE).rdd
+
+    val followee = lines.map(line => (line.split("\t")(0))).distinct()
+    val follower = lines.map(line => (line.split("\t")(1))).distinct()
+
+    val numArr = follower.union(followee).distinct().collect()
+
+    val numNodes = numArr.length
+
+    println("Array of all the users is  "+numArr)
+    println("Total no of all the users is "+numNodes)
+
+    val links = lines.map{ s =>
+      val parts = s.split("\t")
+      (parts(1), parts(0))
+    }.distinct().groupByKey().cache()
+
+    var ranks = links.mapValues(v => 1.0/numNodes)
+
+    for (i <- 1 to iters) {
+      val contribs = links.join(ranks).values.flatMap{ case (urls, rank) =>
+        val size = urls.size
+        if ( size == 0 )
+          numArr.map(url => (url, rank / size))
+        else
+          urls.map(url => (url, rank / size))
+      }
+      ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
+    }
+
+    val output = ranks.collect()
+    output.foreach(tup => println(s"${tup._1} has rank:  ${tup._2} ."))
+
+    spark.stop()
+  }
+}
