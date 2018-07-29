f5fc7d89790e58b9ffa34316be78a8e844376935
Manas Shukla <mshukla@apple.com>
2018-07-28 18:31:29 -0700

    Adding more classes to Kryo Serialisation

--- task2/src/main/scala/PageRank.scala
+++ task2/src/main/scala/PageRank.scala
@@ -115,7 +115,38 @@ object PageRank {
   private def getConfig = {
     val conf = new SparkConf()
     conf.registerKryoClasses(
-      Array(classOf[scala.collection.mutable.WrappedArray.ofRef[_]],Class.forName("PageRank"))
+      Array(
+        classOf[scala.collection.mutable.WrappedArray.ofRef[_]],
+        classOf[org.apache.spark.sql.types.StructType],
+        classOf[Array[org.apache.spark.sql.types.StructType]],
+        classOf[org.apache.spark.sql.types.StructField],
+        classOf[Array[org.apache.spark.sql.types.StructField]],
+        Class.forName("org.apache.spark.sql.types.StringType$"),
+        Class.forName("org.apache.spark.sql.types.LongType$"),
+        Class.forName("org.apache.spark.sql.types.BooleanType$"),
+        Class.forName("org.apache.spark.sql.types.DoubleType$"),
+        Class.forName("[[B"),
+        classOf[org.apache.spark.sql.types.Metadata],
+        classOf[org.apache.spark.sql.types.ArrayType],
+        Class.forName("org.apache.spark.sql.execution.joins.UnsafeHashedRelation"),
+        classOf[org.apache.spark.sql.catalyst.InternalRow],
+        classOf[Array[org.apache.spark.sql.catalyst.InternalRow]],
+        classOf[org.apache.spark.sql.catalyst.expressions.UnsafeRow],
+        Class.forName("org.apache.spark.sql.execution.joins.LongHashedRelation"),
+        Class.forName("org.apache.spark.sql.execution.joins.LongToUnsafeRowMap"),
+        classOf[org.apache.spark.util.collection.BitSet],
+        classOf[org.apache.spark.sql.types.DataType],
+        classOf[Array[org.apache.spark.sql.types.DataType]],
+        Class.forName("org.apache.spark.sql.types.NullType$"),
+        Class.forName("org.apache.spark.sql.types.IntegerType$"),
+        Class.forName("org.apache.spark.sql.types.TimestampType$"),
+        Class.forName("org.apache.spark.sql.execution.datasources.FileFormatWriter$WriteTaskResult"),
+        Class.forName("org.apache.spark.internal.io.FileCommitProtocol$TaskCommitMessage"),
+        Class.forName("scala.collection.immutable.Set$EmptySet$"),
+        Class.forName("scala.reflect.ClassTag$$anon$1"),
+        Class.forName("java.lang.Class"),
+        Class.forName("PageRank")
+      )
     )
   }
 }
